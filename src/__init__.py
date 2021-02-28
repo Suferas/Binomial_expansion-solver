2b810166f8443f2ae8ed98014d8281ab8ade3af2
Suferas <69329902+Suferas@users.noreply.github.com>
2021-02-28 15:14:50 -0300

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,7 +1,7 @@
 from binomial_solver import binomial_solver
 import sympy as sp    
 
-print("----------\n    Enter 1 for Addiction (+) -----> (a + b)\u207f \n    Enter 2 for Subtraction (-) -----> (a - b)\u207f\n----------")
+print("----------\n    Enter 1 for Addition (+) -----> (a + b)\u207f \n    Enter 2 for Subtraction (-) -----> (a - b)\u207f\n----------")
 operation = input("Select the operation:\n")
 value_of_a = input("\nInput the numeric value of each variable of the binomial in the form of (a"+u"\u00B1"+"b)\u207f. \nIf you don't have the numeric value, just input the name of the variable.\n\nValue of a: "  )
 value_of_b = input("Value of b: ")
